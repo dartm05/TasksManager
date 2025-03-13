cd9bfed36c40bb8b96b21eaa3449794937d42de1
Daniella Arteaga <darteag@bancolombia.com.co>
2025-03-13 17:56:47 -0400

    chore: Setup Firebase configuration and credentials

--- server/functions/src/index.ts
+++ server/functions/src/index.ts
@@ -1,19 +1,17 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import { initializeApp } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
+import dotenv from "dotenv";
+dotenv.config();
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+export const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: "task-manager-48639.firebaseapp.com",
+    projectId: "task-manager-48639",
+    storageBucket: "task-manager-48639.firebasestorage.app",
+    messagingSenderId: "69026217505",
+    appId: "1:69026217505:web:8376bc771b1dfc1c6c2d60",
+    measurementId: "G-KHR97ZJPE9"
+};
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+initializeApp(firebaseConfig);
+export const db = getFirestore();
