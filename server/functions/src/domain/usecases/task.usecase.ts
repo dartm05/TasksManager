106bd774299ee64571a6241ada0b2270f61a0d46
Daniella Arteaga <darteag@bancolombia.com.co>
2025-03-13 18:33:33 -0400

    feat: Add base error class and user/task models with use cases

---
+++ server/functions/src/domain/usecases/task.usecase.ts
@@ -0,0 +1,9 @@
+import { ITask } from "../models/task";
+
+export interface ITaskUseCase {
+  create(userId: string, task: ITask): Promise<ITask | undefined>;
+  findAll(userId: string): Promise<ITask[]>;
+  findOne(userId: string, id: string): Promise<ITask | undefined>;
+  update(userId: string, id: string, task: ITask): Promise<ITask | undefined>;
+  remove(userId: string, id: string): Promise<ITask | undefined>;
+}
---
+++ server/functions/src/domain/usecases/user.usecase.ts
@@ -0,0 +1,6 @@
+import { IUser } from "../models/user"; 
+
+export interface IUserUseCase {
+    create(user: IUser): Promise<IUser | undefined>;
+    findUserByEmail(email: string): Promise<IUser | undefined>;
+}
