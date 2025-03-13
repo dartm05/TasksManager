f215bf6731b3b451997afc3025fd040a7a559927
Daniella Arteaga <darteag@bancolombia.com.co>
2025-03-13 19:17:19 -0400

    feat: Add services for user and tasks CRUD

---
+++ server/functions/src/application/services/task.service.ts
@@ -0,0 +1,26 @@
+import { ITask } from "../../domain/models/task";
+import { ITaskUseCase } from "../../domain/usecases/task.usecase";
+
+export class TaskService implements ITaskUseCase {
+  constructor(private taskDrivenAdapter: ITaskUseCase) {}
+
+  create(userId: string, task: ITask): Promise<ITask | undefined> {
+    return this.taskDrivenAdapter.create(userId, task);
+  }
+
+  findAll(userId: string): Promise<ITask[]> {
+    return this.taskDrivenAdapter.findAll(userId);
+  }
+
+  findOne(userId: string, id: string): Promise<ITask | undefined> {
+    return this.taskDrivenAdapter.findOne(userId, id);
+  }
+
+  update(userId: string, id: string, task: ITask): Promise<ITask | undefined> {
+    return this.taskDrivenAdapter.update(userId, id, task);
+  }
+
+  remove(userId: string, id: string): Promise<ITask | undefined> {
+    return this.taskDrivenAdapter.remove(userId, id);
+  }
+}
---
+++ server/functions/src/application/services/user.service.ts
@@ -0,0 +1,13 @@
+import { IUserUseCase } from "../../domain/usecases/user.usecase";
+import { IUser } from "../../domain/models/user";
+
+export class UserService implements IUserUseCase {
+  constructor(private userDrivenAdapter: IUserUseCase) {}
+
+  async create(user: IUser): Promise<IUser | undefined> {
+    return this.userDrivenAdapter.create(user);
+  }
+  async findUserByEmail(email: string): Promise<IUser | undefined> {
+    return this.userDrivenAdapter.findUserByEmail(email);
+  }
+}
