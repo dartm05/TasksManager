b1e7f24ac913e33add66d7206711e7f19568df02
Daniella Arteaga <darteag@bancolombia.com.co>
2025-03-15 23:23:42 -0400

    feat(client): add ProtectedRoute component for route protection

---
+++ client/src/components/auth/ProtectedRoute.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Route, Navigate } from 'react-router-dom';
+
+interface ProtectedRouteProps {
+  component: React.ComponentType<any>;
+  isAuthenticated: boolean;
+  [key: string]: any;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ component: Component, isAuthenticated, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      element={
+        isAuthenticated ? (
+          <Component />
+        ) : (
+          <Navigate to="/login" />
+        )
+      }
+    />
+  );
+};
+
+export default ProtectedRoute;
