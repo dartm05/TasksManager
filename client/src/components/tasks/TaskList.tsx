07d37d261e733f512e6caa2ccaf48074265f9e2b
Daniella Arteaga <darteag@bancolombia.com.co>
2025-03-27 19:12:02 -0400

    feat(client): integrate delete functionality in TaskList component

--- client/src/components/tasks/TaskList.tsx
+++ client/src/components/tasks/TaskList.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { useTasks } from "../../hooks/useTasks";
+import { useAuth } from "../../context/AuthProvider";
 import TaskItem from "./TaskItem";
 
 const TaskList: React.FC = () => {
-  const { tasks, loading, error } = useTasks();
+  const { tasks, removeTask ,loading, error } = useTasks();
 
   if (loading) return <p>Loading tasks...</p>;
   if (error) return <p className="text-red-500">{error}</p>;
   if (!tasks.length) return <p>No tasks found.</p>;
 
-  
+  const handleDeleteTask = async (id: string) => {
+    await removeTask(id);
+  };
 
   return (
     <div className="grid gap-4 p-6 md:grid-cols-2 lg:grid-cols-3">
       {tasks.map((task) => (
-        <TaskItem key={task.id} task={task} />
+        <TaskItem key={task.id} task={task} handleDelete={handleDeleteTask} />
       ))}
     </div>
   );
