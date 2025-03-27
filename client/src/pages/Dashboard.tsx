04f2d0ce10149d4a023bde10cd4915412f461fc1
Daniella Arteaga <darteag@bancolombia.com.co>
2025-03-27 13:41:26 -0400

    feat(client): update respinsive Dashboard layout and style

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -4,20 +4,16 @@ import { useAuth } from "../context/AuthProvider";
 import TaskList from "../components/tasks/TaskList";
 import { PlusCircle } from "lucide-react";
 
-const Dashboard = () => {
+const Dashboard: React.FC = () => {
   const navigate = useNavigate();
-  const { logout } = useAuth();
+  const { user } = useAuth();
 
-  const handleLogout = () => {
-    logout();
-    navigate("/login");
-  };
   const handleAddTask = () => {
     navigate("/add-task");
   };
 
   return (
-    <div className="relative flex flex-col items-center justify-start h-screen bg-yellow-50">
+    <div className="relative flex flex-col items-center justify-start h-screen w-screen overflow-auto bg-yellow-50 bg-[repeating-linear-gradient(transparent_0_19px,_#facc15_20px)]">
       <div className="absolute left-0 top-0 h-full w-16 bg-gray-200">
         <div className="flex flex-col items-center justify-center h-full space-y-4">
           {[...Array(10)].map((_, index) => (
@@ -28,16 +24,16 @@ const Dashboard = () => {
           ))}
         </div>
       </div>
- 
-      <div className="ml-20 mt-10 w-full max-w-4xl p-6 bg-white shadow-lg rounded-lg border border-yellow-300">
-        <div className="flex justify-between items-center mb-6">
+
+      <div className="ml-20 mt-10 h-full w-full max-w-[80vw] min-w-[370px] p-6 bg-white shadow-lg rounded-lg border border-yellow-300 overflow-auto">
+        <div className="flex justify-between min-w-[60vw] items-center mb-6">
           <h1 className="text-3xl font-bold text-yellow-800">Dashboard</h1>
           <Button
             onClick={handleAddTask}
-            className="bg-primary text-white px-4 py-2 rounded-md hover:bg-slate-700 transition"
-            
+            className="bg-slate-600 text-white px-4 py-2 rounded-md hover:bg-slate-500 transition flex items-center"
           >
-            <PlusCircle/>  Add Task
+            <PlusCircle className="w-5 h-5" />
+            <span className="hidden sm:inline ml-2">Add Task</span>
           </Button>
         </div>
         <TaskList />
