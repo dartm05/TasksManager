a640b0954b7dd6ee2bd5504eb8518e7d793693ad
Daniella Arteaga <darteag@bancolombia.com.co>
2025-03-27 20:18:35 -0400

    refactor(authService): replace axios with httpService for API calls

--- client/src/api/authService.ts
+++ client/src/api/authService.ts
@@ -1,26 +1,11 @@
-import axios from "axios";
-import { User } from "../utils/types";
+import api from "./httpService";
 
-const API_BASE_URL = "http://127.0.0.1:5001/task-manager-48639/us-central1/api/users";
-
-export const loginUser = async (email: string): Promise<User> => {
-  try {
-    const response = await axios.get(`${API_BASE_URL}/${email}`);
-    console.log("Login response:", response.data);
-    return response.data;
-  } catch (error) {
-    console.error("Login error:", error);
-    throw new Error("Invalid credentials or server error.");
-  }
+export const loginUser = async (email: string) => {
+  const response = await api.get(`/users/${email}`);
+  return response.data;
 };
 
-export const registerUser = async (email: string): Promise<User> => {
-  try {
-    const response = await axios.post(API_BASE_URL, { email });
-    console.log("Register response:", response.data);
-    return response.data;
-  } catch (error) {
-    console.error("Register error:", error);
-    throw new Error("Server error.");
-  }
-}
+export const registerUser = async (email: string) => {
+  const response = await api.post("/users", { email });
+  return response.data;
+};
