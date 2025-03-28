ea44e6e093bcfd3652c19537b735c25e9c934350
Daniella Arteaga <darteag@bancolombia.com.co>
2025-03-28 12:54:26 -0400

    feat(routes): add 404 error handling for undefined routes

--- server/functions/src/infrastructure/routes.ts
+++ server/functions/src/infrastructure/routes.ts
@@ -4,6 +4,7 @@ import taskApp from "./routes/task.routes";
 import userApp from "./routes/user.routes";
 import {errorHandler} from "./controllers/error.controller";
 import * as functions from "firebase-functions/v1";
+import {Error} from "../domain/errors/base-error";
 
 
 const appRoutes = express();
@@ -11,6 +12,10 @@ const appRoutes = express();
 appRoutes.use(cors());
 appRoutes.use("/", taskApp);
 appRoutes.use("/", userApp);
+
+appRoutes.all("*", (req, res, next) => {
+  next(new Error(404, "Route not found", "The page you are looking for does not exist", "Not Found"));
+});
 
 appRoutes.use(errorHandler);
 
