07411b86cb94e7bf7abb219c227d822a79f42a80
Daniella Arteaga <darteag@bancolombia.com.co>
2025-03-28 04:07:06 -0400

    feat(FormField): add required attribute to textarea and input elements

--- client/src/components/tasks/TaskForm/FormField.tsx
+++ client/src/components/tasks/TaskForm/FormField.tsx
@@ -13,6 +13,7 @@ const FormField: React.FC<FormFieldProps> = ({ id, label, value, onChange, place
           placeholder={placeholder}
           rows={5}
           className="w-full p-2 border border-yellow-300 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-400 focus:border-yellow-400"
+          required
         ></textarea>
       ) : (
         <input
@@ -21,6 +22,7 @@ const FormField: React.FC<FormFieldProps> = ({ id, label, value, onChange, place
           value={value}
           onChange={onChange}
           placeholder={placeholder}
+          required
           className="w-full p-2 border border-yellow-300 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-400 focus:border-yellow-400"
         />
       )}
