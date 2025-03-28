b2c5237eeb1c2ee8d521bd714c92949afbfc568d
Daniella Arteaga <darteag@bancolombia.com.co>
2025-03-28 01:02:18 -0400

    feat(TaskList): refactor TaskList component to accept tasks and delete handler as props

--- client/src/components/tasks/TaskList.tsx
+++ client/src/components/tasks/TaskList.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { useTasks } from "../../hooks/useTasks";
+import { Task } from "../../utils/types";
 import TaskItem from "./TaskItem";
 
-const TaskList: React.FC = () => {
+interface TaskListProps {
+  tasks: Task[];
+  handleDeleteTask: (id: string) => void;
+}
+
+const TaskList: React.FC<TaskListProps> = ({ tasks, handleDeleteTask }) => {
   
-  const { tasks, removeTask  } = useTasks();
-
-  const handleDeleteTask = async (id: string) => {
-    await removeTask(id);
-  };
-
   return (
     <div className="grid gap-4 p-6 md:grid-cols-2 lg:grid-cols-3">
       {tasks.map((task) => (
