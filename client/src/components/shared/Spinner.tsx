6a98022f598465b8be3260e0ec65ee8bec5a8479
Daniella Arteaga <darteag@bancolombia.com.co>
2025-03-27 22:01:33 -0400

    refactor(spinner): update Spinner style

--- client/src/components/shared/Spinner.tsx
+++ client/src/components/shared/Spinner.tsx
@@ -1,5 +1,5 @@
 import { Loader2 } from "lucide-react";  
 
 export const Spinner = ( ) => {
-  return <Loader2 className={`w-24 h-24 sm:w-24 sm:h-24 md:w-32 md:h-32 text-slate-800 bg-slate-400 rounded-full animate-spin`} />;
+  return <Loader2 className={`w-24 h-24 sm:w-24 sm:h-24 md:w-32 md:h-32 text-blue-500 bg-slate-300 rounded-full animate-spin`} />;
 };
