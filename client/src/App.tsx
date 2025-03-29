efc238d7cdd33e584800f9962bb0e1d3937ae852
Daniella Arteaga <darteag@bancolombia.com.co>
2025-03-29 00:07:13 -0400

    fix: update imports from 'react-router-dom' to 'react-router'

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router';
 import Header from './components/layout/Header';
 import Login from 'pages/Login';
 import Register from 'pages/Register';
--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router";
 import React, { useState } from "react";
 import { Button } from "../ui/button";
 import { Menu, X, LogOut } from "lucide-react";
